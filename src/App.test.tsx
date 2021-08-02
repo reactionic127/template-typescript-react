c6f99aa54a6ddb936d14434984f21e4c35779729
reactionic127 <richard.gill127@gmail.com>
2021-08-02 18:43:57 +0000

    added test case

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event';
+import "@testing-library/jest-dom/extend-expect";
 
 describe('<App />', () => {
-  test('render elements without any problem', () => {
-    render(<App />);
-    const wrapper = screen.getByTestId('app-wrapper');
-    expect(wrapper).toBeInTheDocument();
+    test('render elements without any problem', () => {
+        render(<App />);
+        const wrapper = screen.getByTestId('app-wrapper');
+        expect(wrapper).toBeInTheDocument();
+    });
 
-    const input = screen.getByTestId('app-input');
-    expect(input).toBeInTheDocument();
-  });
+    test('text content is wrong', () => {
+        render(<App />);
+        const input = screen.getByTestId('app-input');
+        expect(input).toBeInTheDocument();
+
+        fireEvent.change(input, { target: { value: "test1" } })
+        expect(input.textContent).toBe('test');
+    });
 })
