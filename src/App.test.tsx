d88fed80e920768fed602ca80db0ed7b44a75234
reactionic127 <richard.gill127@gmail.com>
2021-08-06 02:50:32 +0000

    added test case

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,22 +1,51 @@
-import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import App from './App';
+import Calculator from './App';
 import userEvent from '@testing-library/user-event';
 import "@testing-library/jest-dom/extend-expect";
 
-describe('<App />', () => {
-    test('render elements without any problem', () => {
-        render(<App />);
-        const wrapper = screen.getByTestId('app-wrapper');
-        expect(wrapper).toBeInTheDocument();
+describe('String Calculator', () => {
+    test('Given the user input is empty when calculating the sum then it should return zero.', () => {
+        const calculator = new Calculator();
+        const expected = 0;
+        const result = calculator.sum();
+        expect(result).toBe(expected);
     });
-
-    test('text content is wrong', () => {
-        render(<App />);
-        const input = screen.getByTestId('app-input');
-        expect(input).toBeInTheDocument();
-
-        fireEvent.change(input, { target: { value: "test1" } })
-        expect(input).toHaveValue('test');
+    test('Given the user input is one number when calculating the sum then it should return the same number. (example "3" should equal 3)', () => {
+        const calculator = new Calculator();
+        const expected = 3;
+        const result = calculator.sum("3");
+        expect(result).toBe(expected);
+    });
+    test('Given the user input is two numbers when calculating the sum then it should return the sum of those numbers. (example "1,2" should equal 3)', () => {
+        const calculator = new Calculator();
+        const expected = 3;
+        const result = calculator.sum("1,2");
+        expect(result).toBe(expected);
+    });
+    test('Given the user input is an unknown amount of numbers when calculating the sum then it should return the sum of all the numbers. (example "1,2,3" should equal 6)', () => {
+        const calculator = new Calculator();
+        const expected = 6;
+        const result = calculator.sum("1,2,3");
+        expect(result).toBe(expected);
+    });
+    test('Given the user input is multiple numbers with new line and comma delimiters when calculating the sum then it should return the sum of all the numbers. (example "1\n2,3" should equal 6)', () => {
+        const calculator = new Calculator();
+        const expected = 6;
+        const result = calculator.sum("1\n2,3");
+        expect(result).toBe(expected);
+    });
+    test('Given the user input is multiple numbers with a custom single-character delimiter when calculating the sum then it should return the sum of all the numbers. (example “//;\n1;2” should return 3)', () => {
+        const calculator = new Calculator();
+        const expected = 3;
+        const result = calculator.sum("//;\n1;2");
+        expect(result).toBe(expected);
+    });
+    test('Given the user input contains one negative number when calculating the sum then it should throw an exception "negatives not allowed: x" (where x is the negative number).', () => {
+        const calculator = new Calculator();
+        expect(() => calculator.sum("1,-2,3")).toThrow('negatives not allowed: -2')
+    });
+    test('Given the user input contains multiple negative numbers mixed with positive numbers when calculating the sum then it should throw an exception "negatives not allowed: x, y, z" (where x, y, z are only the negative numbers)', () => {
+        const calculator = new Calculator();
+        expect(() => calculator.sum("1,-2,3,-4,6,-6")).toThrow('negatives not allowed: -2, -4, -6')
     });
 })
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
-import React, { useState } from 'react';
-import './App.css';
-
-function App() {
-  const [inputText, setInputText] = useState('');
-
-  const updateState = (event: any) => {
-    setInputText(event.target.value)
-  }
-
-  return (
-    <div data-testid="app-wrapper">
-      <input
-        value={inputText}
-        onChange={(event) => updateState(event)}
-        type="text"
-        data-testid="app-input"
-      />
-    </div>
-  );
+export default class Calculator {
+    sum(str: string) {
+        if (!str) {
+            return 0
+        }
+        const numArr = (str.match(/-?\d+/g) || []).map(s => parseFloat(s))
+        const negative = numArr.filter(n => n < 0)
+        if(negative.length >0) {
+            throw `negatives not allowed: ${negative.join(", ")}`;
+            
+        } 
+        const sum = numArr.reduce((acc, curr) => acc + curr, 0)
+        return sum
+    }
 }
-
-export default App;
